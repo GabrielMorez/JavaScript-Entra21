e00a21093257b6ec0f9f55fa8f190863b237fad5
Gabriel Morez <gabrielmorez@hotmail.com>
2023-09-26 21:46:32 -0300

    querySelector

---
+++ 26-09-2023 - Exercícios/2 - Arvore/script.js
@@ -0,0 +1,9 @@
+// var p1 = document.getElementsByTagName('p')[0];
+//     p1.style.color = 'red';
+
+// document.write(`O conteúdo do primeiro paragrafo é: ${p1.innerHTML}`);
+// document.write(`<br>O conteúdo do primeiro paragrafo é: ${p1.innerText}`);
+
+console.log(document.querySelector('.paragrafo'));
+console.log(document.querySelector('#p2'));
+console.log(document.querySelector('input[type=number]'));
